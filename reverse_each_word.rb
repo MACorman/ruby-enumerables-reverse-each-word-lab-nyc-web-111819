a405fe41af2a06ac2b46c9e214fbbb0c96385ee8
Madeline Corman <madelinecorman@gmail.com>
2019-11-12 20:06:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,3 @@
 def reverse_each_word(sentence)
-  #sentence.split.collect {|word| word.reverse}.join(" ")
-  
-  array = %w[sentence]
-  array.collect {|word| word.reverse}.join(" ")
-  
+  sentence.split.collect {|word| word.reverse}.join(" ")
 end 
